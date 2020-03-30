fd579e9836ba98be22a2cb0eb4d47999f8ec0e46
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-03-29 21:26:38 -0700

    add initial files

---
+++ components/Brick.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header'
+
+export default function Brick {
+  return (
+    <View style={styles.container}>
+      <View style={[{flex: 1}, styles.elementsContainer]}>
+        <View style={styles.top}>
+          <Header/>
+        </View>
+        <View style={styles.middle}>
+
+          </View>
+
+        <View style={styles.bottom}>
+          <TouchableOpacity
+          style={styles.btn}
+          >
+            <Text style={styles.btnText}>
+            Go Home
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+          style={styles.btn}
+          >
+            <Text style={styles.btnText}>
+            Visit Dracula
+            </Text>
+          </TouchableOpacity>
+
+
+
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  middle: {
+    flex: 2,
+    backgroundColor: '#FAA030'
+  },
+  bottom: {
+    flex: 3,
+    backgroundColor: '#32B76C',
+    alignItems: 'center'
+  },
+  textOne: {
+    fontSize: 36,
+textAlign: 'center',
+fontWeight: '100',
+marginBottom: 24
+},
+btn: {
+  backgroundColor: 'firebrick',
+  padding: 9,
+  margin: 5,
+  width: 200,
+
+},
+btnText: {
+  color: 'white',
+  fontSize: 20,
+  textAlign: 'center',
+},
+});
---
+++ components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, StyleSheet,Image } from 'react-native';
+
+
+
+
+const Header = (props) => {
+
+  return (
+
+    <View style={styles.header}>
+
+
+        <Text style={styles.textOne}>The Place Visitor</Text>
+        <Text style={styles.textTwo}>A place where you can visit Dracula or a Brick</Text>
+
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+
+  header: {
+    height: 80,
+    padding: 15,
+    backgroundColor: '#084177',
+
+  },
+  textOne: {
+    color: '#fff',
+    fontSize: 23,
+    textAlign: 'center'
+  },
+  textTwo: {
+    color: '#fff',
+    fontSize: 15,
+    textAlign: 'center'
+  }
+
+
+});
+
+
+export default Header
